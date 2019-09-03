17bf4242124246bb9fa59f070b2bf882d9b0b65c
F4103757 Henrique Fidellis Ferreira <fidellis@bb.com.br>
2019-09-03 14:06:11 -0300

    alt

---
+++ src/Component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default props => (
+    <div>teste {props.label}</div>
+)
---
+++ src/test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Componente from 'Componente'
+
+export default Componente;
