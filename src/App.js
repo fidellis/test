f8a856132ba7509db58ce77ab67cc72f178fb4c1
F4103757 Henrique Fidellis Ferreira <fidellis@bb.com.br>
2019-09-10 15:14:03 -0300

    alt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Teste from 'test-module';
 const teste = require('test-component');
 function App() {
   return (
     <div>
-      Teste
-      {teste()}
+      Teste - {teste()}
+      <Teste />
     </div>
   );
 }
