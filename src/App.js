92b5d73d5031760a24fc4a0824469d8e948f8b8a
F4103757 Henrique Fidellis Ferreira <fidellis@bb.com.br>
2019-09-25 11:05:56 -0300

    alt

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ Array(10).fill().forEach((n , i) => {
     label: `Teste label teste label teste ${i+1}`,
     search: i < 3 ? true : false,
     //align: i < 3 ? 'center' : 'right',
-    type: i >= 3 ? 'NUMBER' : '',   
-    
+    type: i >= 3 ? 'NUMBER' : '',
+    // cellRenderer: ({ row }) => row[2],
+    footer: ({ columnKey, rows }) => columnKey
   }
 });
 const rows = Array(10).fill().map((k,i) => {
@@ -39,13 +40,16 @@ const rows = Array(10).fill().map((k,i) => {
 // const rows = [{"1":"1 - 1","2":"2 - 1","3":"3 - 1","4":"4 - 1","5":"5 - 1","6":"6 - 1","7":"7 - 1","8":"8 - 1","9":"9 - 1","10":"10 - 1"},{"1":"1 - 2","2":"2 - 2","3":"3 - 2","4":"4 - 2","5":"5 - 2","6":"6 - 2","7":"7 - 2","8":"8 - 2","9":"9 - 2","10":"10 - 2"},{"1":"1 - 3","2":"2 - 3","3":"3 - 3","4":"4 - 3","5":"5 - 3","6":"6 - 3","7":"7 - 3","8":"8 - 3","9":"9 - 3","10":"10 - 3"}]
 
 const App = () => {
+
   return (
     <div className="teste">
       <ExportDataTable 
           rows={rows} 
           columns={columns}
           width="90%"
-          actions={[{ label: 'Teste'}, `${rows.length} registros`]}
+          actions={[{ label: 'Teste'}, { label: 'Teste 2'}, { label: 'Teste 3'}, { label: 'Teste 4'}, `${rows.length} registros`]}
+          title={<h3>Teste data table Teste data table</h3>}
+          footerHeight={50}
           // maxHeight={500}
           //headerStyle={{ background: 'grey' }}
           //style={{ background: 'grey' }}
